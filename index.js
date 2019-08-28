1cc7cdabd0435020bfceaa602eca2ab6539636b8
Clechay <luke.clechay@gmail.com>
2019-08-28 21:05:54 +0200

    refresh after 4 years

--- index.js
+++ index.js
@@ -1,188 +1,129 @@
-var chalk = require('chalk');
-var inquirer = require("inquirer");
+const chalk = require('chalk');
+const inquirer = require("inquirer");
 
-var core={
-  c_devOut:false,
-  c_colors:{
-    info:function (text) {return chalk.blue(text);},
-    form:function (text) {return chalk.magenta(text);},
-    success:function (text) {return chalk.green(text);},
-    warning:function (text) {return chalk.red(text);},
-    error:function (text) {return chalk.white.bgRed(text);}
-  },
-  c_apply_color:function(text,color) {
-    return this.c_colors[color](text);
-  },
-  c_add_color:function(id,gen) {
-    c_colors[id]=gen;
-  },
-  c_out:function(text,color) {
-    console.log(chalk.bold(this.c_apply_color(text,color)));
-  },
-  c_dev:function(text,color) {
-    if(this.c_devOut) this.c_out(text,color);
+let display_development_output = false;
+const colors = {
+}
+const defaultColors = {
+  info:    (...input) => chalk.blue(...input),
+  form:    (...input) => chalk.magenta(...input),
+  success: (...input) => chalk.green(...input),
+  warning: (...input) => chalk.red(...input),
+  error:   (...input) => chalk.white.bgRed(...input),
+}
+const apply_color = (colorId, ...input) => {
+  const brush = colors[colorId] || console.log;
+  return brush(...input);
+}
+const out = (colorId, ...input) => {
+  console.log(apply_color(colorId, ...input));
+}
+const dev = (...args) => {
+  if(display_development_output) out(...args);
+}
+
+const set_color = (colorId,brush) => {
+  colors[colorId]=brush;
+  out[colorId] = (...input)=>out(colorId, ...input)
+  dev[colorId] = (...input)=>dev(colorId, ...input)
+}
+
+for (const colorId in defaultColors) {
+  if (defaultColors.hasOwnProperty(colorId)) {
+    set_color(colorId,defaultColors[colorId])    
   }
 }
-module.exports={
-  setDevMode:function(remote_devMode_val){
-  	c_devOut=remote_devMode_val;
-  	return c_devOut;
-  },
-  devOut:{
-    err:function (text,color){
-        if(typeof color==="undefined") color='error';
-        core.c_dev(text,color);
-    },
-    warn:function (text,color){
-        if(typeof color==="undefined") color='warning';
-        core.c_dev(text,color);
-    },
-    info:function (text,color){
-        if(typeof color==="undefined") color='info';
-        core.c_dev(text,color);
-    },
-    log:function (text,color){
-        if(typeof color==="undefined") color='info';
-        core.c_dev(text,color);
-    },
-    success:function (text,color){
-        if(typeof color==="undefined") color='success';
-        core.c_dev(text,color);
-    },
-    form:function (text,color){
-        if(typeof color==="undefined") color='form';
-        core.c_dev(text,color);
-    }
-  },
-  out:{
-    err:function (text,color){
-        if(typeof color==="undefined") color='error';
-        core.c_out(text,color);
-    },
-    warn:function (text,color){
-        if(typeof color==="undefined") color='warning';
-        core.c_out(text,color);
-    },
-    info:function (text,color){
-        if(typeof color==="undefined") color='info';
-        core.c_out(text,color);
-    },
-    log:function (text,color){
-        if(typeof color==="undefined") color='info';
-        core.c_out(text,color);
-    },
-    success:function (text,color){
-        if(typeof color==="undefined") color='success';
-        core.c_out(text,color);
-    },
-    form:function (text,color){
-        if(typeof color==="undefined") color='form';
-        core.c_out(text,color);
-    }
-  },
-  in:{
-    text:function (rem_question,rem_default,rem_validate) {
-      var questions = [
-        {
-          type: "input",
-          name: "inp",
-          message: rem_question
-        }
-      ];
-      if (typeof rem_default!=="undefined") {
-        if (typeof rem_default==="function") {
-          questions[0].default=rem_def;
-        }
-        else if (typeof rem_default==="string") {
-          questions[0].default=function () { return rem_def; };
-        }
+
+const input = {
+  text:(questionText,defaultValue,validator) => {
+      const question = {
+        type: "input",
+        name: "inp",
+        message: questionText
+      };
+
+      if (["string","number","boolean","array","function"].includes(defaultValue))
+        question.default=defaultValue;
+        
+      if (typeof validator==="function") {
+        question.validate=validator;
       }
-      if (typeof rem_validate!=="undefined") {
-        if (typeof rem_validate==="function") {
-          questions[0].validate=rem_validate;
-        }
-        // else if (typeof rem_validate==="string") {
-        //
-        // }
-      }
-      var response;
-      inquirer.prompt( questions, function( answers ) {
+
+      let response;
+      inquirer.prompt( [question], function( answers ) {
         response=answers;
       });
       require('deasync').loopWhile(function(){return !response;});
       return response.inp;
     },
-    list:function (rem_question,rem_choices) {
-      var questions = [
+    list:function (questionText,choices) {
+      const question =
         {
           type: "list",
           name: "inp",
-          choices: rem_choices,
-          message: rem_question
+          choices,
+          message: questionText
         }
-      ];
-      var response;
-      inquirer.prompt( questions, function( answers ) {
+      ;
+      let response;
+      inquirer.prompt( [question], function( answers ) {
         response=answers;
       });
       require('deasync').loopWhile(function(){return !response;});
       return response.inp;
     },
-    checkbox:function (rem_question,rem_choices) {
-      var questions = [
-        {
-          type: "checkbox",
-          name: "inp",
-          choices: rem_choices,
-          message: rem_question
-        }
-      ];
-      var response;
-      inquirer.prompt( questions, function( answers ) {
+    checkbox:function (questionText,choices) {
+      const question = {
+        type: "checkbox",
+        name: "inp",
+        choices,
+        message: questionText
+      };
+      let response;
+      inquirer.prompt( [question], function( answers ) {
         response=answers;
       });
       require('deasync').loopWhile(function(){return !response;});
       return response.inp;
     },
-    confirm:function (rem_question) {
-      var questions = [
-        {
-          type: "confirm",
-          name: "inp",
-          message: rem_question
-        }
-      ];
-      var response;
-      inquirer.prompt( questions, function( answers ) {
+    confirm:function (questionText) {
+      const question = {
+        type: "confirm",
+        name: "inp",
+        message: questionText
+      };
+      let response;
+      inquirer.prompt( [question], function( answers ) {
         response=answers;
       });
       require('deasync').loopWhile(function(){return !response;});
       return response.inp;
     },
-    pass:function (rem_question,rem_validate) {
-      var questions = [
-        {
-          type: "password",
-          name: "inp",
-          message: rem_question
-        }
-      ];
-      if (typeof rem_validate!=="undefined") {
-        if (typeof rem_validate==="function") {
-          questions[0].validate=rem_validate;
-        }
+    pass:function (questionText,validator) {
+      const questions = {
+        type: "password",
+        name: "inp",
+        message: questionText
       }
-      var response;
+      if (typeof validator==="function") {
+        question.validate=validator;
+      }
+      let response;
       inquirer.prompt( questions, function( answers ) {
         response=answers;
       });
       require('deasync').loopWhile(function(){return !response;});
       return response.inp;
     }
+}
+
+module.exports={
+  setDevelopmentMode:(active)=>{
+    display_development_output = active;
+    return active;
   },
-  utils:{
-    apply_color:function(text,color) {
-      return c_apply_color(text,color);
-    }
-  }
+  out,
+  dev,
+  apply_color,
+  in:input
 }
